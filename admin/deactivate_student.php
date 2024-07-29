e043a8d0f3ae975673a27e03232082d6c40ca6d3
youssef mostafa <yomu611@gmail.com>
2024-07-29 18:33:56 +0300

    Update admin scripts: modified deactivate_student.php and others

--- admin/deactivate_student.php
+++ admin/deactivate_student.php
@@ -29,22 +29,13 @@ if ($_SESSION['REQUEST_METHOD'] == 'POST') {
         $conn->close();
         exit;
     }
-    $remove = $conn->prepare("SELECT user_id from stdata where student_name = ? and grade = ? and section = ? LIMIT 1");
-    $remove->bind_param('sis', $name, $grade, $section);
-    $remove->execute();
-    $remove->store_result();
-    $remove->bind_result($user_id);
-    $remove->fetch();
-    $remove = $conn->prepare("DELETE FROM stdata where student_name = ? and grade = ? and section = ?");
-    $remove->bind_param("sis", $name, $grade, $section);
-    $remove->execute();
-    $remove = $conn->prepare("DELETE FROM stdssn where user_id = ?");
-    $remove->bind_param('i', $user_id);
-    $remove->execute();
     $table_name = 'G' . "$grade" . 'S' . "$section" . "-attendence";
-    $remove = $conn->prepare("DELETE FROM $table_name WHERE user_id = ?");
-    $remove->bind_param('i', $user_id);
-    $remove->execute();
-    $remove->close();
+    $deactve = $conn->prepare("UPDATE stdata SET status='inactive' WHERE student_name=? and grade=? and section=?");
+    $deactve->bind_param('sis', $name, $grade, $section);
+    $deactve->execute();
+    $deactve = $conn->prepare("UPDATE $table_name SET student_status='inactive' WHERE student_name=?");
+    $deactve->bind_param('s', $name);
+    $deactve->execute();
+    $deactve->close();
     $conn->close();
 }
