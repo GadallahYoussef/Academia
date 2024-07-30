715d7c8f2d67e5a15bf635f7d86175cfabc53581
youssef mostafa <yomu611@gmail.com>
2024-07-30 23:56:05 +0300

    hola

--- admin/remove_grade.php
+++ admin/remove_grade.php
@@ -15,32 +15,67 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
     $table_name = 'G' . "$grade" . 'S' . "$section" . "-attendence";
-    $remove = $conn->prepare("DROP TABLE IF EXISTS $table_name");
-    $remove->execute();
-    $remove->close();
-    $gather = $conn->prepare("SELECT user_id from stdata WHERE grade=? and section=?");
-    $gather->bind_param('is', $grade, $section);
-    $gather->execute();
-    $gather->store_result();
-    $gather->bind_result($user_id);
-    if ($gather->num_rows > 0) {
-        $grade_students = [];
-        while ($gather->fetch()) {
-            $grade_students[] = $user_id;
+    $conn->begin_transaction();
+    try {
+        $remove = $conn->prepare("DROP TABLE IF EXISTS $table_name");
+        if ($remove->execute()) {
+            $remove->close();
+        } else {
+            $remove->close();
+            throw new Exception("database delete error");
         }
-        $gather->close();
+        $gather = $conn->prepare("SELECT user_id from stdata WHERE grade=? and section=?");
+        $gather->bind_param('is', $grade, $section);
+        if ($gather->execute()) {
+            $gather->store_result();
+            $gather->bind_result($user_id);
+            if ($gather->num_rows > 0) {
+                $grade_students = [];
+                while ($gather->fetch()) {
+                    $grade_students[] = $user_id;
+                }
+                $gather->close();
+            }
+        } else {
+            $gather->close();
+            throw new Exception("database load error");
+        }
         $remove = $conn->prepare("DELETE from stdata Where grade= ? and section= ?");
         $remove->bind_param('is', $grade, $section);
-        $remove->execute();
+        if ($remove->execute()) {
+            $remove->close();
+        } else {
+            $remove->close();
+            throw new Exception("database remove error");
+        }
         $remove = $conn->prepare("DELETE from classes Where grade= ? and section= ?");
         $remove->bind_param('is', $grade, $section);
-        $remove->execute();
+        if ($remove->execute()) {
+            $remove->close();
+        } else {
+            $remove->close();
+            throw new Exception("grade remove error");
+        }
         $remove = $conn->prepare("DELETE FROM stdssn WHERE user_id= ?");
         foreach ($grade_students as $student) {
             $remove->bind_param('s', $student);
-            $remove->execute();
+            if (!$remove->execute()) {
+                $remove->close();
+                throw new Exception("sessions remove error");
+            }
         }
         $remove->close();
+        $conn->commit();
+        echo json_encode(['status' => 'OK', 'message' => 'Deleted Successfully']);
+        exit;
+    } catch (Exception $e) {
+        $conn->rollback();
+        $conn->close();
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+        exit;
     }
+} else {
+    $conn->close();
+    echo json_encode(['status' => 'error', 'message' => 'Invalid Request']);
+    exit;
 }
-$conn->close();
